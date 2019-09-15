989e8e1532f504b3794ce8d65c88b2d67ec39f78
Nicolas Lemahieu <44812328+Nicolas-lmh@users.noreply.github.com>
2019-09-15 20:56:46 +0200

    Add files via upload
    Fixed ES6 version of API OC

---
+++ src/ajax.js
@@ -0,0 +1,43 @@
+// Execute an AJAX GET request
+// First argument is URL's target, second is a callback function in case of success
+const ajaxGet = (url, callback) => {
+    const req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", () => {
+        if (req.status >= 200 && req.status < 400) {
+            // Means the success of the request, applies the callback function to the response
+            callback(req.responseText);
+        } else {
+            console.error(`${req.status} ${req.statusText} ${url}`);
+        }
+    });
+    req.addEventListener("error", () => {
+        console.error(`Network error on the following url: ${url} `);
+    });
+    req.send(null);
+}
+
+// Execute an AJAX POST request
+// Second argument data is a javascript object
+// isJson argument is a true or false value which means if data argmuent is a JSON object
+const ajaxPost = (url, data, callback, isJson) => {
+    const req = new XMLHttpRequest();
+    req.open("POST", url);
+    req.addEventListener("load", () => {
+        if (req.status >= 200 && req.status < 400) {
+            callback(req.responseText);
+        } else {
+            console.error(`${req.status} ${req.statusText} ${url}`);
+        }
+    });
+    req.addEventListener("error", () => {
+        console.error(`Network error on the following url: ${url}`);
+    });
+    if (isJson) {
+        // Define the request à a JSON one
+        req.setRequestHeader("Content-Type", "application/json");
+        // Convert JSON data as a string before its sending
+        data = JSON.stringify(data);
+    }
+    req.send(data);
+}
